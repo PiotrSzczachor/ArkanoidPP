8586b36d3f175dea0c862003bc5b3fd63d16859f
PiotrSzczachor1 <piotr.szczachor1@wp.pl>
2021-06-04 01:19:04 +0200

    First Commit

---
+++ ProjektPP/__recovery/ArkanoidPP.h
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------
+
+#ifndef ArkanoidPPH
+#define ArkanoidPPH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TShape *Background;
+	TImage *Ball;
+	TTimer *BallTimer;
+	TImage *Image1;
+	void __fastcall BallTimerTimer(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
